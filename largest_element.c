056d349f09704903816783e5b777be1ed315bfc4
syedmiran-38 <miranhussain4@gmail.com>
2023-11-06 07:52:54 +0530

    6th Nov

---
+++ largest_element.c
@@ -0,0 +1,16 @@
+// Program: Find the largest element in an array
+#include <stdio.h>
+
+int main() {
+    int arr[] = {25, 11, 7, 75, 56};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int max = arr[0];
+
+    for (int i = 0; i < length; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Largest element present in the given array: %d\n", max);
+    return 0;
+}
